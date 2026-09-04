8994bb48a298dfb93efefa182d262095777c2f73
juniorsousa53339-svg <juniorsousa53339@gmail.com>
2026-09-04 13:28:20 -0300

    chore: atualizei dependências e coloquei a versão java 17

--- backend/src/main/java/br/com/duxusdesafio/model/Integrante.java
+++ backend/src/main/java/br/com/duxusdesafio/model/Integrante.java
@@ -1,11 +1,18 @@
 package br.com.duxusdesafio.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.Objects;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 @Table(name = "integrante")
 public class Integrante {
 	
@@ -23,49 +30,6 @@ public class Integrante {
 	
 	@OneToMany(mappedBy = "integrante")
 	private List<ComposicaoTime> composicaoTime;
-
-
-	public Integrante() {
-	}
-
-	public Integrante(String nome, String funcao, List<ComposicaoTime> composicaoTime) {
-		this.nome = nome;
-		this.funcao = funcao;
-		this.composicaoTime = composicaoTime;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getFuncao() {
-		return funcao;
-	}
-
-	public void setFuncao(String funcao) {
-		this.funcao = funcao;
-	}
-
-	public List<ComposicaoTime> getComposicaoTime() {
-		return composicaoTime;
-	}
-
-	public void setComposicaoTime(List<ComposicaoTime> composicaoTime) {
-		this.composicaoTime = composicaoTime;
-	}
-
 
 	@Override
 	public boolean equals(Object o) {
